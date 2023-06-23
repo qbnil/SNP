86379e7cc622a54e8beb3aa4602e39e930d598cc
qbnil <tomederson92@gmail.com>
2023-06-23 17:00:14 +0300

    task number 1 completed

---
+++ task_01.py
@@ -0,0 +1,13 @@
+import re
+def is_palindrome(txt) -> str:
+    if txt is not None and type(txt) is not int:
+        squashed_string = re.sub(r'[^\w\s]', '', txt.lower()).replace(" ", "")
+        # print(squashed_string[::-1])
+        return squashed_string==squashed_string[::-1]
+    else:
+        return False
+
+print(is_palindrome("A man, a plan, a canal -- Panama"))
+# print(is_palindrome("Madam, I'm Adam!"))
+# print(is_palindrome("Abracadabra"))
+# print(is_palindrome(None))
