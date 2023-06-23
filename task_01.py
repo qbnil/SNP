67ccd2fada213d8f3f2fbbf1e43e096b065fc872
qbnil <tomederson92@gmail.com>
2023-06-24 00:00:39 +0300

    updated the 1 file

--- task_01.py
+++ task_01.py
@@ -1,5 +1,5 @@
 import re
-def is_palindrome(txt) -> str:
+def is_palindrome(txt):
     if txt is not None and type(txt) is not int:
         squashed_string = re.sub(r'[^\w\s]', '', txt.lower()).replace(" ", "")
         # print(squashed_string[::-1])
