b672f965b1888c41c6e7d8cddb506aa0e471e11e
qbnil <tomederson92@gmail.com>
2023-06-24 22:33:53 +0300

    the 5 task is updated

--- task_05.py
+++ task_05.py
@@ -2,12 +2,15 @@ import datetime
 import time
 
 def data_in_future(days):
+    if isinstance(days, (int)):
+        tday = datetime.date.today()
 
-    tday = datetime.date.today()
+        needed_data = tday + datetime.timedelta(days)
 
-    needed_data = tday + datetime.timedelta(days)
+        return "{} {}".format(needed_data.strftime("%d-%m-%Y"), time.strftime("%H:%M:%S"))
+    else:
+        tday_default = datetime.date.today()
+        return f"{tday_default.strftime('%d-%m-%Y')} {time.strftime('%H:%M:%S')}"
 
-    return "{} {}".format(needed_data.strftime("%d-%m-%Y"), time.strftime("%H:%M:%S"))
-
-
-# print(data_in_future(2))
+# print(data_in_future(None))
+# print(data_in_future(3))
